2cf72bb70ce13237900fadc202b4225ea9805b3a
Sosuke Brause <brosuke89@gmail.com>
2020-04-20 14:36:57 -0700

    modified js added first function

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,14 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var chars = "abcdefghkmnpqrstuvwxyz23456789";
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  for (var i = 1; i < 4; i++) {}
   passwordText.value = password;
-
 }
-
+console.log("hello");
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
