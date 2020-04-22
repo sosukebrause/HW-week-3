91f3244053be01302319460409a88e694bc72c8d
Sosuke Brause <brosuke89@gmail.com>
2020-04-21 17:18:36 -0700

    adding number array and for loop

--- Develop/script.js
+++ Develop/script.js
@@ -9,6 +9,18 @@ function writePassword() {
   for (var i = 1; i < 4; i++) {}
   passwordText.value = password;
 }
+
+function 
+
+function generateAlphabet () {
+  var character = [];
+
+  for (let n=0; n<10; n++) {
+    character.push(n.toString());
+  }
+}
+
+
 console.log("hello");
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
