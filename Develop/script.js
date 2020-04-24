ed60b7ee12a92caf061faf09617975646b237fea
Sosuke Brause <brosuke89@gmail.com>
2020-04-23 19:51:02 -0700

    link btnSubmit and changed array

--- Develop/script.js
+++ Develop/script.js
@@ -1,27 +1,47 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
-var chars = "abcdefghkmnpqrstuvwxyz23456789";
+var chars = "abcdefghijklmnopqrstuvwxyz123456789!@#$%^&*_-/+?";
+console.log(chars.length);
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-  for (var i = 1; i < 4; i++) {}
-  passwordText.value = password;
-}
+  if (password != undefined) {
+    var passwordText = document.querySelector("#password");
 
-function 
-
-function generateAlphabet () {
-  var character = [];
-
-  for (let n=0; n<10; n++) {
-    character.push(n.toString());
+    passwordText.value = password;
   }
 }
 
+function generatePassword() {
+  var character = [];
+  var length = prompt("Enter character length numerically");
+  if (length > 128 || length < 8) {
+    alert("must be more than 8 and less than 128");
+    return;
+  } else if (isNaN(length) === true) {
+    alert("must provide number for length");
+    return;
+  } else {
+    for (let n = 0; n < length; n++) {
+      character.push(chars[Math.floor(Math.random() * chars.length - 1)]);
+    }
+    return character.join("");
+  }
+}
+function add() {
+  console.log("hello");
+}
+var generateBtn = document.querySelector("#generate");
 
-console.log("hello");
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-console.log(tests);
+
+// console.log(generateAlphabet());
+// document.getElementById("password").innerHTML = generatePassword();
+//innerHTML is function to add dynamically
+
+// for (let n = 0; n < 10; n++) {
+//   console.log(Math.floor(Math.random() * 10));
+// }
+
+// Add event listener to generate button
+// console.log(tests);
